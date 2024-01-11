5ccdd887d6794fd4fba7622928d84bbd7fc90c0f
Serhii Rybak <serhiirybak@Serhiis-MacBook-Pro.local>
2024-01-11 23:03:20 +0100

    Added gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+
+// Additional styles import
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+renderGalleryItems(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+/**
+ * Renders gallery items
+ * @param galleryItems
+ */
+function renderGalleryItems(galleryItems) {
+  galleryContainer.innerHTML = galleryItems.map(getImageHtml).join("");
+}
+
+/**
+ * Returns gallery item html
+ * @param galleryItem
+ * @returns
+ */
+function getImageHtml({ preview, original, description }) {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+          </li>`;
+}
