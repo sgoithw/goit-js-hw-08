7edff10a2aab07ece13945ab5cfa0ab8216bd7cc
Serhii Rybak <serhiirybak@Serhiis-MacBook-Pro.local>
2024-01-11 23:37:22 +0100

    Tackled second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import VimeoPlayer from "@vimeo/player";
+import _ from "lodash";
 
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+const iframe = document.querySelector("iframe");
+const player = new VimeoPlayer(iframe);
+const time = localStorage.getItem(LOCALSTORAGE_KEY);
+const onTimeupdate = _.throttle((e) => {
+  localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
+}, 1000);
+
+time && player.setCurrentTime(time);
+
+player.on("timeupdate", onTimeupdate);
