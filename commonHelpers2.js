282fa25d0c22f90c6b4faf6c03666b9e13a4cc45
sgoithw <141962074+sgoithw@users.noreply.github.com>
2024-01-13 20:41:22 +0000

    Deploying to gh-pages from @ sgoithw/goit-js-hw-08@a8a6cfde818ceba5104ecdc9f39316f160e7012a 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as m,_ as a}from"./assets/vendor-a09c4a83.js";const t="videoplayer-current-time",n=document.querySelector("iframe"),o=new m(n),e=localStorage.getItem(t),c=a.throttle(r=>{localStorage.setItem(t,r.seconds)},1e3);e&&o.setCurrentTime(e);o.on("timeupdate",c);
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as m,t as a}from"./assets/vendor-58ef91fa.js";const t="videoplayer-current-time",n=document.querySelector("iframe"),o=new m(n),e=localStorage.getItem(t),c=a(r=>{localStorage.setItem(t,r.seconds)},1e3);e&&o.setCurrentTime(e);o.on("timeupdate",c);
 //# sourceMappingURL=commonHelpers2.js.map
